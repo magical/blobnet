ec5f6659d5babab1d5d79d6f67b8a82fb0b7b066
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-04-21 15:10:06 -0700

    more cleanup

--- Search.c
+++ Search.c
@@ -171,10 +171,12 @@ int main(int argc, const char* argv[]) {
         pthread_join(threadIDs[t], NULL);
     }
 
+    unsigned long numSeeds = lastSeed - firstSeed + 1;
     clock_t time_b = clock();
+    double duration = time_b - time_a;
 
-    printf("searched %ld seeds in %f ms\n", lastSeed-firstSeed+1, (time_b-time_a) * (1e3 / CLOCKS_PER_SEC));
-    printf("average %.1f us/seed\n", (time_b-time_a) * (1e6 / CLOCKS_PER_SEC) / (lastSeed-firstSeed+1));
+    printf("searched %lu seeds in %f ms\n", numSeeds, duration * (1e3 / CLOCKS_PER_SEC));
+    printf("average %.1f us/seed\n", duration * (1e6 / CLOCKS_PER_SEC) / numSeeds);
 }
 
 static void* searchPools(void* args) {
